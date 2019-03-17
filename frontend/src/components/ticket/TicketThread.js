4d6b3cd22a197e0a5cd4d18eecb6be9c8e876227
zt <zhaotong_tan@mymail.sutd.edu.sg>
2019-03-18 00:58:10 +0800

    Added reply box

--- frontend/src/components/ticket/TicketThread.js
+++ frontend/src/components/ticket/TicketThread.js
@@ -5,6 +5,8 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TicketMessages from './TicketMessages';
 import uuid from 'uuid';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 
 const styles = theme => ({
     root: {
@@ -20,7 +22,72 @@ const messagesTest = [
         { name: 'User1', message: 'I have a problem' },
         { name: 'Admin1', message: 'Okay solve your problem' }];
 
+const RESET_VALUES = {
+    name: 'User1',
+    message: '',
+};
+
 class TicketThread extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            reply: Object.assign({}, RESET_VALUES),
+        };
+    }
+
+    handleSubmit(e) {
+        console.log('clicked submit');
+        // let ticketVaild = this.handleValidation(e);
+        console.log('finish checking');
+
+        // if (ticketVaild) {
+        //     //this.addTicket(e);
+        //     console.log(JSON.stringify(e));
+        //     this.addTicket(e);
+        //     alert("Ticket has been created.")
+        //     // reset the form values to blank after submitting:
+        //     this.setState({
+        //         ticket: Object.assign({}, RESET_VALUES),
+        //     });
+        //     this.props.history.push('/');
+        //     //return <Redirect to='/' push={true}></Redirect>;
+        // } else {
+        //     alert("Please fill in all the required fills.");
+        // }
+        // // prevent the form submit event from triggering an HTTP Post:
+        // e.preventDefault();
+
+    }
+
+    handleValidation(e) {
+        let reply = e;
+        if (reply.message === '') {
+            console.log('No messages');
+            return false;
+        }
+        // if (ticket.category === '') {
+        //     console.log('No category');
+        //     return false;
+        // }
+        // if (ticket.message === '') {
+        //     console.log('No message');
+        //     return false;
+        // }
+        return true;
+    }
+
+    handleChange(e) {
+        const value = e.value;
+        const name = 'User1';
+
+        this.setState((prevState) => {
+            prevState.reply.message = value;
+            prevState.reply[name] = name;
+            return { reply: prevState.reply };
+        });
+    }
 
     render() {
         console.log('inside a thread');
@@ -57,9 +124,28 @@ class TicketThread extends React.Component {
                         messages={messagesTest}>
                     </TicketMessages>
                 </div>
+                <div>
+                        <TextField
+                            id="outlined-full-width"
+                            label="Reply"
+                            name="Reply"
+                            multiline
+                            rowsMax="4"
+                            value={this.state.reply.message}
+                            onChange={this.handleChange}
+                            fullWidth
+                            margin="normal"
+                            variant="outlined"
+                            // required="required"
+                            />
+                        <Button
+                            variant="contained"
+                            onClick={this.handleSubmit.bind(this, this.state.reply)}
+                            >
+                            Submit
+                          </Button>
+                </div>
             </React.Fragment>
-
-
         );
     }
 }
