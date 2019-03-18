b22704d2ca0fd58dce674b2fbb6a5a32b8af8e01
zt <zhaotong_tan@mymail.sutd.edu.sg>
2019-03-18 22:58:45 +0800

    reposting of reply working

--- frontend/src/components/ticket/TicketThread.js
+++ frontend/src/components/ticket/TicketThread.js
@@ -7,6 +7,7 @@ import TicketMessages from './TicketMessages';
 import uuid from 'uuid';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
+import $ from 'jquery';
 
 const styles = theme => ({
     root: {
@@ -18,10 +19,14 @@ const styles = theme => ({
     },
 });
 
+/*
+// This constant was to test the display of messages
 const messagesTest = [
-        { name: 'User1', message: 'I have a problem'},
-        { name: 'Admin1', message: 'Okay solve your problem'}];
+        { name: 'User1', message: 'I have a problem', data: ''},
+        { name: 'Admin1', message: 'Okay solve your problem', data: ''}];
+*/
 
+// Values to be resetted to after the user has click a submit button and succeeded
 const RESET_VALUES = {
     name: 'User1',
     message: '',
@@ -40,74 +45,73 @@ class TicketThread extends React.Component {
         };
     }
 
+    // returns the date and time the reply was posted in UTC
     getDateCreated(){
         var today = new Date();
         console.log(today);
         return today.toUTCString();
     }
 
+    // API call to update the ticket
     addReply(e) {
         // getting the ticket variables for PUT
-        var objectId = this.props.location.state.ticket.objectId;
+        let objectId = this.props.location.state.ticket.objectId;
         var replies = this.props.location.state.ticket.replies;
         console.log(objectId);
         console.log(replies);
 
+        // add the date
         e.date =  this.getDateCreated();
         console.log(e);
 
         // adding the new reply to the original
         replies.push(e);
         console.log(replies);
+        let data = { "replies":replies};
+        console.log(JSON.stringify(data));
         // console.log(JSON.stringify(replies));
-        
-        // changing the url
-        let objString = "/swivel/acnapi-common-services/common/classes/Tickets/" + objectId.toString();
-        var a = new URL("https://ug-api.acnapiv3.io");
-        console.log(a);
-        var b = new URL(objString, "https://ug-api.acnapiv3.io");
-        // https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/classes/Tickets/2aPEA53ynY
-        // const url = new URL("https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/classes/Tickets");
-        // let address = 'https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/classes/Tickets';
-        console.log(b);
-        
-        fetch(b,
-            {
-                method: 'PUT',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
-                    "cache-control": "no-cache",
-                },
-                body: JSON.stringify(replies)
-            }).then(res => res.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/classes/Tickets/" + objectId,
+            "method": "PUT",
+            "headers": {
+              "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
+              "Content-Type": "application/json",
+              "cache-control": "no-cache",
+            },
+            "processData": false,
+            "data": JSON.stringify(data)
+          }
+          
+          $.ajax(settings).done(function (response) {
+            console.log("reply added succesfully");
+          });
     
     }
 
+    // handles event when submit button is clicked
     handleSubmit(e) {
         console.log('clicked submit');
         let ticketVaild = this.handleValidation(e);        
         console.log('finish checking');
         if (ticketVaild) {
             this.addReply(e);
-        //     this.addTicket(e);
-        //     alert("Ticket has been created.")
-        //     // reset the form values to blank after submitting:
-        //     this.setState({
-        //         ticket: Object.assign({}, RESET_VALUES),
-        //     });
-        //     this.props.history.push('/');
-        //     //return <Redirect to='/' push={true}></Redirect>;
-        // } else {
-        //     alert("Please fill in all the required fills.");
+            alert("Your reply has been posted.")
+            // reset the form values to blank after submitting:
+            this.setState({
+                ticket: Object.assign({}, RESET_VALUES),
+            });
+            this.props.history.push('/');
+            return <Redirect to='/' push={true}></Redirect>;
         }
-        // // prevent the form submit event from triggering an HTTP Post:
-        // e.preventDefault();
+        // prevent the form submit event from triggering an HTTP Post:
+        e.preventDefault();
 
     }
 
+    // check if there are valid messages in the reply box
     handleValidation(e) {
         let reply = e;
         console.log(e);
@@ -126,6 +130,7 @@ class TicketThread extends React.Component {
         return true;
     }
 
+    // reflect changes in the text box when the user is typing
     handleChange(e) {
         const value = e.target.value;
         const name = 'User1';
