39d1b97253800098438e9a9b19a6d22283b113e0
zt <zhaotong_tan@mymail.sutd.edu.sg>
2019-03-18 20:31:29 +0800

    added reply

--- frontend/src/components/ticket/TicketThread.js
+++ frontend/src/components/ticket/TicketThread.js
@@ -37,6 +37,21 @@ class TicketThread extends React.Component {
         };
     }
 
+    addReply(e) {
+        fetch('https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/classes/Tickets',
+            {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
+                    "cache-control": "no-cache",
+                },
+                body: JSON.stringify(e)
+            }).then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    }
+
     handleSubmit(e) {
         console.log('clicked submit');
         // let ticketVaild = this.handleValidation(e);
@@ -93,9 +108,6 @@ class TicketThread extends React.Component {
         console.log('inside a thread');
         return (
             <React.Fragment>
-                <p>
-
-                </p>
                 <div>
                     <Paper elevation={5}>
                         <Typography align="left" variant="subtitle1">
@@ -112,8 +124,6 @@ class TicketThread extends React.Component {
                         </Typography>
                     </Paper>
                 </div>
-                <p>
-                </p>
                 <div>
                     <p align='left'>
                         Message:<br />
