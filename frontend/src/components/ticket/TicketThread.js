f53cf18e65e73bb44fe0acb1b16345f12cec1a3f
zt <zhaotong_tan@mymail.sutd.edu.sg>
2019-03-19 14:19:02 +0800

    added account icon

--- frontend/src/components/ticket/TicketThread.js
+++ frontend/src/components/ticket/TicketThread.js
@@ -237,7 +237,13 @@ class TicketThread extends React.Component {
                 </div>
                 <div>
                     <Typography align="left" variant="subtitle1">
-                            Message:<br />
+                        <p>
+
+                        </p>
+                            Messages:<br />
+                        <p>
+
+                        </p>
                     </Typography>
                         <TicketMessages
                             key={uuid.u4}
@@ -255,7 +261,7 @@ class TicketThread extends React.Component {
                             value={this.state.reply.message}
                             onChange={this.handleChange}
                             fullWidth
-                            style={{ paddingLeft: 20, paddingTop: 10, paddingBottom: 10}}
+                            style={{ paddingLeft: 20, paddingTop: 10, paddingBottom: 10, paddingRight:20}}
                             margin="normal"
                             variant="outlined"
                             InputLabelProps={{shrink: true,}}
