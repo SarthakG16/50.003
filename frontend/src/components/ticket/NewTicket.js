b7245158e3db71f8863aff039b5fc9ea9f7da76d
zt <zhaotong_tan@mymail.sutd.edu.sg>
2019-03-19 17:34:20 +0800

    checking params test

--- frontend/src/components/ticket/NewTicket.js
+++ frontend/src/components/ticket/NewTicket.js
@@ -45,10 +45,11 @@ export default class NewTicket extends React.Component {
                         }
 
                     ],
-            status:'open'
+            status:'open',
+            email: e.email
         }
         console.log(data);
-
+        /*
         fetch('https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/classes/Tickets',
             {
                 method: 'POST',
@@ -62,7 +63,7 @@ export default class NewTicket extends React.Component {
             .then(data => console.log(data))
             .catch(err => console.log(err));
 
-        
+        */
     }
 
     handleSubmit(e) {
@@ -80,7 +81,7 @@ export default class NewTicket extends React.Component {
             this.sendNotif(e);
 
             // window.location.herf = '/';
-            this.props.history.push('/');
+            // this.props.history.push('/');
             // reset the form values to blank after submitting:
             this.setState({
                 ticket: Object.assign({}, RESET_VALUES),
@@ -133,20 +134,38 @@ export default class NewTicket extends React.Component {
     //need help
     sendNotif(e){
         let emailBody = {
-            "subject": "test subject using ACNAPI",
-            "sender": "sarthakganoorkar@gmail.com",
-            "recipient": e.email,
+            "subject":"test subject using ACNAPI",
+            "sender":"sarthakganoorkar@gmail.com",
+            "recipient": "zhaotong_tan@mymail.sutd.edu.sg",
             "html": "<h1>HELLO!</h1>"
         }
+        console.log(emailBody);
+        console.log(JSON.stringify(emailBody));
+
+        
+        fetch("https://ug-api.acnapiv3.io/swivel/email-services/api/mailer",
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
+                    "cache-control": "no-cache"
+                },
+                body: JSON.stringify(emailBody)
+            }).then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+
+        /*
         var settings = {
             "async": true,
             "crossDomain": true,
             "url": "https://ug-api.acnapiv3.io/swivel/email-services/api/mailer",
             "method": "POST",
             "headers": {
-              "Content-Type": "application/json",
-              "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
-              "cache-control": "no-cache",
+                "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
+                "Content-Type": "application/json",
+                "cache-control": "no-cache",
             },
             "processData": false,
             "data": JSON.stringify(emailBody)
@@ -154,7 +173,7 @@ export default class NewTicket extends React.Component {
           
           $.ajax(settings).done(function (response) {
             console.log("Email sent");
-          });
+          });*/
     }
 
     render() {
