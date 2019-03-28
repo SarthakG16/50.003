4dc4a11108b65e5cb747493f8f79bb72c0218c66
SarthakG16 <sarthakganoorkar@gmail.com>
2019-03-29 00:30:40 +0800

    No longer default email

--- frontend/src/components/ticket/NewTicket.js
+++ frontend/src/components/ticket/NewTicket.js
@@ -13,7 +13,6 @@ const RESET_VALUES = {
     email: '',
     status: 'Open'
 };
-// var request = require("request");
 
 const RESET_VALUES_ERROR = { title: '', message: '', category: '', email: '' };
 
@@ -27,7 +26,7 @@ export default class NewTicket extends React.Component {
             errorText: (Object.assign({}, RESET_VALUES_ERROR)),
             user: this.props.location.user
         };
-        //console.log(this.state.user);
+        console.log(this.state.user);
     }
 
     // returns the date and time the reply was posted in UTC
@@ -182,9 +181,9 @@ export default class NewTicket extends React.Component {
     //need help
     sendNotif(e) {
         let emailBody = {
-            "subject": "test subject using ACNAPI",
-            "sender": "sarthakganoorkar@gmail.com",
-            "recipient": "zhaotong_tan@mymail.sutd.edu.sg",
+            "subject": "Test subject using ACNAPI",
+            "sender": "sarthakganoothrkar@gmail.com",
+            "recipient": e.email,
             "html": "<h1>HELLO!</h1>"
         }
         console.log(emailBody);
@@ -222,33 +221,6 @@ export default class NewTicket extends React.Component {
           $.ajax(settings).done(function (response) {
             console.log("Email sent");
           });*/
-
-        //   var options2 = {
-        //     method: "POST",
-        //     url: "https://ug-api.acnapiv3.io/swivel/email-services/api/mailer",
-        //     headers: {
-        //       "cache-control": "no-cache",
-        //       "Content-Type": "application/json",
-        //       "Server-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJWVkpYS1lmZkdNdFZBRUwwYjFuVmNVcUFYY2IwZzhrM0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MzgsImV4cCI6MTU1MjU0NDkzOCwiYXpwIjoiVlZKWEtZZmZHTXRWQUVMMGIxblZjVXFBWGNiMGc4azMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.XYoNbl50Gyuk7xNPK64GZLEdNMs18uAf4sFMiQn6lOUv3tw0espP5avymr-GsFXgnl2kugClsb_ybBkuSvchqp8dvvL1dyejiumyZCTw0FluNWqGqiNJb4mGTEeNRUCxexgrTm5yV2ZxPNFpfumD44GLYBaW_EVJden3hi9XJ8UpD1MrXuZD8YUEtZ_sHKS9bcZxSJoyqbu3n7l0p0K_q74FSY34xwey2SpbX3Zipng5Mk2KYlw0L6kMiJSsmChgerG_gWkSGjhM8mcuURGtCYTxucEyuaxmBI8kNP7VuvGXYBwiAcL2dH7FSES09XKZS7z0ie5ax_vvO4JoLxztgw",
-        //     },
-        //     body: {
-        //       subject: "New issue added by ",
-        //       sender: "ZhiYao@mymail.accenture.com",
-        //       recipient: "jhyeep@gmail.com",
-        //       html:
-        //         "<h3>" +
-        //         "There is a new issue: " +
-        //         " posted by " +
-        //         "</h3></h3>" +
-        //         " Content of the issue is " +
-        //         "</h3>"
-        //     },
-        //     json: true
-        //   };
-        //   request(options2, function(error, response, body) {
-        //     if (error) throw new Error(error);
-        //     console.log(body);
-        //   });
     }
 
 
