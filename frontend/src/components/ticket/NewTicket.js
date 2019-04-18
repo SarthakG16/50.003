0218b44637ee7ffa7684d399240767e4c08f2cb5
jhyeep <45350427+jhyeep@users.noreply.github.com>
2019-04-19 00:34:12 +0800

    Admin no ticket limit

--- frontend/src/components/ticket/NewTicket.js
+++ frontend/src/components/ticket/NewTicket.js
@@ -145,12 +145,12 @@ export default class NewTicket extends React.Component {
         }
         // var nextDay = +now > +lastTicketDate;
         // if (this.state.user.numberOfTickets < ticketLimit || nextDay) {
-        if ((this.numberOfTickets < ticketLimit || nextDay) && e.message.length <= this.maxMessageChars) {
-            //reset numberOfTickets if nextDay, and increment by 1
-            // if (this.props.location.state.isAdmin) {
-            //     console.log("Admin has no ticket limit");
-            // }
-            if (nextDay) {
+        if ((this.numberOfTickets < ticketLimit || nextDay || this.isAdmin) && e.message.length <= this.maxMessageChars) {
+            // reset numberOfTickets if nextDay, and increment by 1
+            if (this.isAdmin) {
+                console.log("Admin has no ticket limit");
+            }
+            else if (nextDay) {
                 this.numberOfTickets = 1;
                 this.changeField("numberOfTickets", this.numberOfTickets);
             } else {
