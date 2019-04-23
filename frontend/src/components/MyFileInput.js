ed0393dfc6cf511cd6efe58de085a15e33955854
jhyeep <45350427+jhyeep@users.noreply.github.com>
2019-04-23 13:47:22 +0800

    Revert "small fix for fakepath problem"
    This reverts commit 8e58cbc88c412c8b258bb542ef233dff96d19d31.

--- frontend/src/components/MyFileInput.js
+++ frontend/src/components/MyFileInput.js
@@ -18,7 +18,7 @@ class MyFileInput extends React.Component {
                             style={{ display: "none" }}
                             accept={fileType}
                             onChange={() => {
-                                document.getElementById("fileField").innerHTML = document.getElementById("fileInput").files[0].name;
+                                document.getElementById("fileField").innerHTML = document.getElementById("fileInput").value;
                             }}
                         />
                     </Button>
